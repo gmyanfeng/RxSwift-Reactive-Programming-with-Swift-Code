4817f3fc079e38089b524d572b53cb3dd183490c
zhengyanfeng <zhengyanfeng@haylou.com>
2021-01-16 14:37:00 +0800

    add deferred demo code

--- Chapter2_Observables/Chapter2_Observables/ViewController.swift
+++ Chapter2_Observables/Chapter2_Observables/ViewController.swift
@@ -6,15 +6,42 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        deferredDemo()
     }
-    
-    
+}
 
+extension ViewController {
+    func deferredDemo() {
+        example(of: "deferred") {
+            let disposeBag = DisposeBag()
+            
+            var flip = false
+            let factory = Observable<Int>.deferred({
+                flip.toggle()
+                if flip {
+                    return Observable.of(1,2,3)
+                }else{
+                    return Observable.of(4,5,6)
+                }
+            })
+            
+            for _ in 0...3 {
+                factory.subscribe(onNext: {
+                    print($0, terminator: "")
+                })
+                .disposed(by: disposeBag)
+                
+                print()
+            }
+            
+        }
+    }
 }
 
